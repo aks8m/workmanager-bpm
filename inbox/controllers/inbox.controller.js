f7d92b00e5e3001bb37a85fa22ac20cc5290bc36
aks8m <andrew.k.sills@gmail.com>
2016-01-12 15:07:05 -0500

    Changes made to add more functionality to Inbox Page

--- inbox/controllers/inbox.controller.js
+++ inbox/controllers/inbox.controller.js
@@ -1,65 +1,116 @@
 'use strict';
 
-angular.module('workmanagerBPMApp').controller('InboxController', function (Inbox, localStorageService, User) {
+angular.module('workmanagerBPMApp').controller('InboxController', function (Inbox, localStorageService, User, $scope) {
     var vm = this;
+    var columnArray = ['number', 'assignment', 'domain', 'createdBy', 'dateAssigned', 'status', 'details'];
 
 
     vm.gridOptions = {
+        enableRowSelection: true,
         enableSorting: true,
+        noUnselect: true,
+        showGridFooter: false,
+        multiSelect: false,
+        enableFullRowSelection: true,
+        enableRowHeaderSelection: false,
+        onRegisterApi: function (gridApi) {
+            vm.gridApi = gridApi;
+            vm.gridApi.grid.registerRowsProcessor(vm.singleFilter, 200);
+            vm.gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+                vm.selectedAssignment = row.entity;
+            });
+        },
         columnDefs: [
             {
                 name: 'Assignment Number',
                 field: 'number',
-                enableCellEdit: false
+                enableCellEdit: false,
+                width: "**"
             },
             {
                 name: 'Assignment',
                 field: 'assignment',
-                enableCellEdit: false
+                enableCellEdit: false,
+                width: "**"
             },
             {
                 name: 'Domain',
                 field: 'domain',
-                enableCellEdit: false
+                enableCellEdit: false,
+                width: "**"
             },
             {
                 name: 'Created By',
                 field: 'createdBy',
-                enableCellEdit: false
+                enableCellEdit: false,
+                width: "**"
             },
             {
-                name: 'Date Assigned',
+                name: 'Date',
                 field: 'dateAssigned',
                 enableCellEdit: false
             },
             {
                 name: 'Number of Terms',
                 field: 'termCount',
-                enableCellEdit: false
+                enableCellEdit: false,
+                width: "**"
             },
             {
                 name: 'Terms Validated',
                 field: 'validatedCount',
-                enableCellEdit: false
+                enableCellEdit: false,
+                width: "**"
             },
             {
                 name: 'Status',
                 field: 'status',
-                enableCellEdit: false
+                enableCellEdit: false,
+                width: "**"
             },
             {
                 name: 'Billable Hours',
                 field: 'billableHours',
-                enableCellEdit: false
+                enableCellEdit: false,
+                width: "**"
             },
             {
                 name: 'Details',
                 field: 'details',
-                enableCellEdit: false
+                enableCellEdit: false,
+                width: "**"
             }
         ],
         data: Inbox.getData(User.getUser().id)
     };
+
+    vm.singleFilter = function (renderableRows) {
+        var matcher = new RegExp(vm.searchFilterValue);
+        renderableRows.forEach(function (row) {
+            var match = false;
+            columnArray.forEach(function (field) {
+                if (String(row.entity[field]).match(matcher)) {
+                    match = true;
+                }
+            });
+            if (!match) {
+                row.visible = false;
+            }
+        });
+        return renderableRows;
+    };
+
+
+    vm.filter = function () {
+        vm.gridApi.grid.refresh();
+    };
+
+    vm.resetFilter = function () {
+        vm.searchFilterValue = "";
+        vm.filter();
+
+    };
+
 
 
 });
--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,3 @@
 'use strict';
 
-var myApp = angular.module('workmanagerBPMApp', ['ui.router', 'ui.grid', 'ui.bootstrap', 'LocalStorageModule', 'ui.grid.edit']);
+var myApp = angular.module('workmanagerBPMApp', ['ui.router', 'ui.grid', 'ui.bootstrap', 'LocalStorageModule', 'ui.grid.edit', 'ui.grid.selection','ui.grid.exporter']);
